f55dacb98b56adfd05da3d504db1c2b86d38690a
Ak-AlphaData <akshatatadakod@gmail.com>
2024-09-28 16:11:36 +0530

    Added another Project to Github

---
+++ Walmart Sales Data Analysis/Sales.sql
@@ -0,0 +1,37 @@
+-- ---------------------------- Sales ---------------------------------
+
+-- Number of sales made in each time of the day per each weekday 
+SELECT
+	time_of_day,
+	COUNT(*) AS total_sales
+FROM sales
+WHERE day_name IN ('Sunday' ,'monday','tuesday','wensday','thursday','friday','saturday')
+GROUP BY time_of_day 
+ORDER BY total_sales DESC;
+-- Evenings experience most sales, the stores are 
+-- filled during the evening hours
+
+-- Which of the customer types brings the most revenue?
+SELECT
+	customer_type,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY customer_type
+ORDER BY total_revenue desc;
+
+-- Which city has the largest tax/VAT (Value Added Tax) percent?
+SELECT
+	city,
+    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
+FROM sales
+GROUP BY city 
+ORDER BY avg_tax_pct DESC;
+
+-- Which customer type pays the most in VAT(Value added tax)?
+SELECT
+	customer_type,
+	AVG(tax_pct) AS total_tax
+FROM sales
+GROUP BY customer_type
+ORDER BY total_tax;
+
